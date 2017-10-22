4620cf72d783f329fe75cd649fc01fb8806e154e
k24dizzle <zhaok24@uw.edu>
2017-10-22 12:53:37 -0700

    Working app

--- project/app.py
+++ project/app.py
@@ -8,10 +8,12 @@ from datetime import datetime as dt
 from datetime import timedelta
 from twitter import getTweets
 import logging
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__);
+json_return = None
 @app.route('/api/trump', methods=['GET'])
 def tweets():
     test = json.dumps(getTweets(20));
@@ -25,19 +27,44 @@ def dump(obj):
   for attr in dir(obj):
     print("obj.%s = %s" % (attr, getattr(obj, attr)))
 
-@app.route('/api/text', methods=['POST', 'GET'])
+@app.route('/api/text/', methods=['POST','GET'])
 def text():
-    print("Request to api/text")
-    # I'm guessing we want to parse text here
-    parsed_dict = parse_text(json.loads(request.data), ['Foris Kuang', 'Andrew Li']);
-    print(parsed_dict)
+    global json_return
+    if(request.method == 'POST'):
+        print("Request to api/text")
+        print(request.args.get('username'))
+        print(request.args.get('recipient'))
+        # I'm guessing we want to parse text here
+        parsed_dict = parse_text(json.loads(request.data), [request.args.get('username'), request.args.get('recipient')]);
+        print(parsed_dict)
 
-    cog_results = {}
-    for k, v in parsed_dict.iteritems():
-        cog_results[k] = cog_api_call(v)
-        print cog_results[k]
-        
-    return 'cjuss'
+        cog_results = {}
+        for k, v in parsed_dict.iteritems():
+            cog_results[k] = cog_api_call(v)
+
+        # change cog_results[k] to a json object to send to front end
+        # front end then grabs dict values to display graph. Also, analyze
+        # polyfit and include in json
+
+        x = []
+        y = []
+
+        # TODO: Fix this so it actually gets a value we want (not hard code)
+        analyze_user = request.args.get('username')
+
+        for k, v in cog_results.iteritems() :
+            if k[0] is analyze_user :
+                x.append(k[2])
+                y.append(v['documents'][0]['score'])
+        serializable_cog_results = [{'key':k, 'value': v} for k, v in cog_results.iteritems()]
+        best_fit = np.polyfit(x, y, 4).tolist()
+        json_result = {"results": serializable_cog_results, "best_fit": best_fit}
+        print json_result
+        json_return = json.dumps(json_result)
+        return render_template('index.html')
+    else:
+        print(json_return)
+        return json_return
 
 '''
 @app.route('/api/message/<parsed_dict>', methods=['GET'])
@@ -102,7 +129,7 @@ def parse_text(text_file, users):
                 remove_end_p = remove_p.replace("</p>", "")
                 concat_messages += remove_end_p + " "
             if len(concat_messages) != 0 :
-                msgs_between[(user_thread.people, str(time_start))] = concat_messages
+                msgs_between[(user_thread.people, str(time_start), 20 - i)] = concat_messages
     return msgs_between
 
 def cog_api_call(text):
@@ -128,9 +155,8 @@ def cog_api_call(text):
     conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, json.dumps(body), headers)
     response = conn.getresponse()
     data = response.read()
-    print(data)
     conn.close()
-    return json.dumps(data)
+    return json.loads(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
